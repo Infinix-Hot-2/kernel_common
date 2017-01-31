fb943992a75b67a106d9ee4147601f3c2e0aa0ae
Daniel Rosenberg <drosen@google.com>
2017-01-31 14:24:32 -0800

    ANDROID: ion: Fix up error handling
    Error handling here seems to have rotted over time.
    This cleans it up and corrects some ordering.
    
    Bug: 34283718
    Change-Id: I5c12b56294c77ce109866f0c7ab4ed5a74fdc616
    Signed-off-by: Daniel Rosenberg <drosen@google.com>

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -196,13 +196,13 @@ static struct ion_buffer *ion_buffer_create(struct ion_heap *heap,
 
 	if (ret) {
 		if (!(heap->flags & ION_HEAP_FLAG_DEFER_FREE))
-			goto err2;
+			goto err_free_buffer;
 
 		ion_heap_freelist_drain(heap, 0);
 		ret = heap->ops->allocate(heap, buffer, len, align,
 					  flags);
 		if (ret)
-			goto err2;
+			goto err_free_buffer;
 	}
 
 	buffer->dev = dev;
@@ -213,8 +213,8 @@ static struct ion_buffer *ion_buffer_create(struct ion_heap *heap,
 			"heap->ops->map_dma should return ERR_PTR on error"))
 		table = ERR_PTR(-EINVAL);
 	if (IS_ERR(table)) {
-		ret = -EINVAL;
-		goto err1;
+		ret = PTR_ERR(table);
+		goto err_heap_free;
 	}
 
 	buffer->sg_table = table;
@@ -226,7 +226,7 @@ static struct ion_buffer *ion_buffer_create(struct ion_heap *heap,
 		buffer->pages = vmalloc(sizeof(struct page *) * num_pages);
 		if (!buffer->pages) {
 			ret = -ENOMEM;
-			goto err;
+			goto err_heap_unmap_dma;
 		}
 
 		for_each_sg(table->sgl, sg, table->nents, i) {
@@ -260,11 +260,11 @@ static struct ion_buffer *ion_buffer_create(struct ion_heap *heap,
 	mutex_unlock(&dev->buffer_lock);
 	return buffer;
 
-err:
+err_heap_unmap_dma:
 	heap->ops->unmap_dma(heap, buffer);
-err1:
+err_heap_free:
 	heap->ops->free(buffer);
-err2:
+err_free_buffer:
 	kfree(buffer);
 	return ERR_PTR(ret);
 }
