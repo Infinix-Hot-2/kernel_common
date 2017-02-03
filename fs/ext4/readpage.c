610e33853a17f6210408fb75bffcf6f5736408cb
Mohan Srinivasan <srmohan@google.com>
2017-02-03 15:48:03 -0800

    ANDROID: Refactor fs readpage/write tracepoints.
    ANDROID: Refactor the fs readpage/write tracepoints to move the
    inode->path lookup outside the tracepoint code, and pass a pointer
    to the path into the tracepoint code instead. This is necessary
    because the tracepoint code runs non-preemptible. Thanks to
    Trilok Soni for catching this in 4.4.
    
    Signed-off-by: Mohan Srinivasan <srmohan@google.com>

--- fs/ext4/readpage.c
+++ fs/ext4/readpage.c
@@ -150,13 +150,18 @@ ext4_submit_bio_read(struct bio *bio)
 {
 	if (trace_android_fs_dataread_start_enabled()) {
 		struct page *first_page = bio->bi_io_vec[0].bv_page;
+		char *path, pathbuf[MAX_TRACE_PATHBUF_LEN];
 
+		path = android_fstrace_get_pathname(pathbuf,
+						    MAX_TRACE_PATHBUF_LEN,
+						    first_page->mapping->host);
 		if (first_page != NULL) {
 			trace_android_fs_dataread_start(
 				first_page->mapping->host,
 				page_offset(first_page),
 				bio->bi_iter.bi_size,
 				current->pid,
+				path,
 				current->comm);
 		}
 	}
