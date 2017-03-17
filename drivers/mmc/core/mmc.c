ea2ae661ea592a201557c26cfeddd1055dfdb95e
Jin Qian <jinqian@google.com>
2017-03-17 14:20:52 -0700

    ADNROID: mmc: core: export device health info via sysfs
    Change-Id: I055b36c5bc0d4920fc3e060bf3b71c26d809c018
    Test: cat /sys/bus/mmc/devices/mmc0:0001/rev
    Bug: 36228467
    Signed-off-by: Jin Qian <jinqian@google.com>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -592,6 +592,11 @@ static int mmc_decode_ext_csd(struct mmc_card *card, u8 *ext_csd)
 		card->ext_csd.ffu_capable =
 			(ext_csd[EXT_CSD_SUPPORTED_MODE] & 0x1) &&
 			!(ext_csd[EXT_CSD_FW_CONFIG] & 0x1);
+		card->ext_csd.pre_eol_info = ext_csd[EXT_CSD_PRE_EOL_INFO];
+		card->ext_csd.lifetime_est_a =
+			ext_csd[EXT_CSD_DEV_LIFE_TIME_EST_TYP_A];
+		card->ext_csd.lifetime_est_b =
+			ext_csd[EXT_CSD_DEV_LIFE_TIME_EST_TYP_B];
 	}
 out:
 	return err;
@@ -721,6 +726,10 @@ MMC_DEV_ATTR(manfid, "0x%06x\n", card->cid.manfid);
 MMC_DEV_ATTR(name, "%s\n", card->cid.prod_name);
 MMC_DEV_ATTR(oemid, "0x%04x\n", card->cid.oemid);
 MMC_DEV_ATTR(prv, "0x%x\n", card->cid.prv);
+MMC_DEV_ATTR(rev, "%u\n", card->ext_csd.rev);
+MMC_DEV_ATTR(pre_eol_info, "%u\n", card->ext_csd.pre_eol_info);
+MMC_DEV_ATTR(lifetime_est_a, "%u\n", card->ext_csd.lifetime_est_a);
+MMC_DEV_ATTR(lifetime_est_b, "%u\n", card->ext_csd.lifetime_est_b);
 MMC_DEV_ATTR(serial, "0x%08x\n", card->cid.serial);
 MMC_DEV_ATTR(enhanced_area_offset, "%llu\n",
 		card->ext_csd.enhanced_area_offset);
@@ -757,6 +766,10 @@ static struct attribute *mmc_std_attrs[] = {
 	&dev_attr_name.attr,
 	&dev_attr_oemid.attr,
 	&dev_attr_prv.attr,
+	&dev_attr_rev.attr,
+	&dev_attr_pre_eol_info.attr,
+	&dev_attr_lifetime_est_a.attr,
+	&dev_attr_lifetime_est_b.attr,
 	&dev_attr_serial.attr,
 	&dev_attr_enhanced_area_offset.attr,
 	&dev_attr_enhanced_area_size.attr,
